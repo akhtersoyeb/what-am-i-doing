1cd9a44c28afd347de226126586a4d688595f40f
sk soyeb akhter <sksoyebakhter2003@gmail.com>
2022-07-05 19:38:40 +0530

    changes the background color

---
+++ constants/Colors.ts
@@ -0,0 +1,12 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+
+export default {
+  background: '#023047',
+  primaryColor: '#041A24',
+  secondaryColor: '#032739',
+  primaryText: '#E5E5E5',
+  secondaryText: '#457B9D'
+};
+
