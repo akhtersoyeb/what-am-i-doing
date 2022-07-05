6b80d5cd60110e62c5789e32d4c1283c2e7ef7a0
sk soyeb akhter <sksoyebakhter2003@gmail.com>
2022-07-05 20:07:51 +0530

    enables us to use patrick-hand as text's fontFamily

--- App.tsx
+++ App.tsx
@@ -1,15 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
+import useCachedResources from "./hooks/useCachedResources";
 import Colors from "./constants/Colors";
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  const isLoadingComplete = useCachedResources();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.defaultText}>
+          Open up App.tsx to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,5 +27,8 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.background,
     alignItems: "center",
     justifyContent: "center",
+  },
+  defaultText: {
+    fontFamily: "patrick-hand",
   },
 });
